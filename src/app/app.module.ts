230723df65ab46a2cd84f37845e74abcfad3f951
Holland <seamus.m.holland@metlife.com>
2019-07-11 10:46:00 -0400

    Fixed imports bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
 import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
