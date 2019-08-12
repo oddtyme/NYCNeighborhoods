d2c8a08980deeb520785a54ed8643477835a36d0
Holland <seamus.m.holland@metlife.com>
2019-08-12 08:54:49 -0400

    Set zoom to appropriate level

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { GoogleMapsAPIWrapper, AgmMap, LatLngBounds, LatLngBoundsLiteral} from '@agm/core';
+import { ViewChild } from '@angular/core';
+
+declare var google: any;
 
 @Component({
   selector: 'app-root',
@@ -9,4 +13,5 @@ export class AppComponent {
   title = 'NYCNeighborhoods';
   lat: number = 40.7128;
   lng: number = -74.0060;
+  zoom: number = 10.75;
 }
